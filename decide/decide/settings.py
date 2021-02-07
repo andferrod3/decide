bfd6ff794308eb7cc56adb35681c5f3ccfb2b544
egc <andferrod3@alum.us.es>
2021-02-07 09:57:17 +0000

    modificado los archivos heroku
    cambiado los archivos para desplegar heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -165,6 +165,8 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
+BASEURL = 'http://entreno-examen.herokuapp.com'
+
 try:
     from local_settings import *
 except ImportError:
@@ -180,3 +182,9 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+APIS = {}
+
+import django_heroku
+django_heroku.settings(locals())
+
